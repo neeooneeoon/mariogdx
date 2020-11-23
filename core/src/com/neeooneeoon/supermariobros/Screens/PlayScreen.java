248a2d81a731bc0da586256a954b0bde51a5112f
Ngoc Nguyen <30312571+neeooneeoon@users.noreply.github.com>
2020-11-24 00:39:12 +0700

    part 3 finished

---
+++ core/src/com/neeooneeoon/supermariobros/Screens/PlayScreen.java
@@ -0,0 +1,67 @@
+package com.neeooneeoon.supermariobros.Screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.neeooneeoon.supermariobros.SuperMarioBros;
+
+public class PlayScreen implements Screen {
+    private SuperMarioBros game;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+    Texture texture;
+
+    public PlayScreen(SuperMarioBros game) {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(1280,720, gameCam);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
+        game.batch.begin();
+        game.batch.draw(texture,0,0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
